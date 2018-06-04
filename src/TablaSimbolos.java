e3aa903255f246a49566b5fcc36e30b2b80f7f06
Roberto <iruga090@gmail.com>
2018-06-04 18:46:51 -0500

    First commit

---
+++ src/TablaSimbolos.java
@@ -0,0 +1,36 @@
+
+public class TablaSimbolos {
+	private int valor;
+	private String tipo;
+	
+	public TablaSimbolos() {}
+	
+	public int  Buscar(String v) {
+		return 0; 
+	}
+	
+	public int Insertar(String v, int t) {
+		return 0;
+	}
+	
+	public void FijarTipo(int i, int n_t) {
+		
+	}
+	
+	public int IndicarTipo(int i) {
+		return 0;
+	}
+	
+	public String IndicarValor(int i) {
+		return "";
+	}
+	
+	public void ImprimeTS() {
+		
+	}
+	
+	public void Inicializa() {
+		
+	}
+
+}
